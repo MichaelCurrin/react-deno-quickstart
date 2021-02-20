a5101f2aa68d6c69ed57a38991e681a35c2a25dc
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-20 18:30:35 +0200

    feat: Create server.jsx

---
+++ server.jsx
@@ -0,0 +1,28 @@
+/**
+ * Server module.
+ *
+ * Start a dev server for React app. This works independently of an HTML page.
+ */
+import { App } from "./App.jsx";
+import { React } from "./deps.ts";
+import { Application, ReactDOMServer } from "./dev_deps.ts";
+
+const APP_PORT = 8080;
+
+const app = new Application();
+
+app.use((next) =>
+  (c) => {
+    let e = next(c);
+    if (React.isValidElement(e)) {
+      e = ReactDOMServer.renderToString(e);
+    }
+
+    return e;
+  }
+);
+
+app.get("/", App)
+  .start({ port: APP_PORT });
+
+console.log(`Dev server listening on http://localhost:${APP_PORT}`);
