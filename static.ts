e0cb3030a8ee1caf5bd35aff4e3b817b4be0db43
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-20 18:30:38 +0200

    feat: Create static.ts

---
+++ static.ts
@@ -0,0 +1,18 @@
+/**
+ * Static module.
+ *
+ * Start a static server in the build output directory. Useful for testing a production build
+ * locally before a deploy.
+ */
+// @ts-ignore TS complains about extension with ts(2691) but Deno needs it.
+import { Application } from "./dev_deps.ts";
+
+const APP_PORT = 80;
+
+const app = new Application();
+
+app.file("/", "build/index.html")
+  .static("/", "build")
+  .start({ port: APP_PORT });
+
+console.log(`Server listening on http://localhost:${APP_PORT}`);
