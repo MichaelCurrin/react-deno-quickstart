9577b21c665829e54a324a398c550f265bd3599a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-21 13:00:53 +0200

    docs: Add comments

--- deps.ts
+++ deps.ts
@@ -1,2 +1,5 @@
+/**
+ * Production dependencies module.
+ */
 export { default as React } from "https://dev.jspm.io/react@17.0.1";
 export { default as ReactDOM } from "https://dev.jspm.io/react-dom@17.0.1";
