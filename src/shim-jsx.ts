859cb487ec0124edc61f5ac73f433f81d3d1addf
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-21 19:13:03 +0200

    Update shim-jsx.ts

--- src/shim-jsx.ts
+++ src/shim-jsx.ts
@@ -1,26 +1,6 @@
 declare namespace JSX {
   interface IntrinsicElements {
     // deno-lint-ignore no-explicit-any
-    link: any;
-    // deno-lint-ignore no-explicit-any
-    header: any;
-    // deno-lint-ignore no-explicit-any
-    h1: any;
-    // deno-lint-ignore no-explicit-any
-    h2: any;
-    // deno-lint-ignore no-explicit-any
-    div: any;
-    // deno-lint-ignore no-explicit-any
-    p: any;
-    // deno-lint-ignore no-explicit-any
-    i: any;
-    // deno-lint-ignore no-explicit-any
-    a: any;
-    // deno-lint-ignore no-explicit-any
-    img: any;
-    // deno-lint-ignore no-explicit-any
-    button: any;
-    // deno-lint-ignore no-explicit-any
-    br: any;
+    [elemName: string]: any;
   }
 }
