78677432e766ed56107802df6c07098e70638e9d
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-02-23 12:22:06 +0200

    style: Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ export function App() {
         <GHBadge username="MichaelCurrin" repoName="react-deno-quickstart" />
       </header>
 
-      <Counter/>
+      <Counter />
     </div>
   </div>;
 }
