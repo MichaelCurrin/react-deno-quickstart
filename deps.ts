bfb7ef8b0d45793675bed4e30d73c82ce1543ec5
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-18 11:29:05 +0200

    fix: Make sure to match versions across deps files

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 /**
  * Production dependencies module.
  */
-export { default as React } from "https://dev.jspm.io/react@17.0.2";
-export { default as ReactDOM } from "https://dev.jspm.io/react-dom@17.0.2";
+export { default as ReactDOM } from "https://dev.jspm.io/react-dom@17.0.1";
+export { default as React } from "https://dev.jspm.io/react@17.0.1";
