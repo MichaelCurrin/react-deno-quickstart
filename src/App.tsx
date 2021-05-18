c7e3aa4166391d350a981d4318bf9a832554749c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-18 12:14:19 +0200

    style: Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,29 @@
  * Note - shims must come **before** components so it can be applied there.
  */
 import { React } from "../deps.ts";
-import "./shims-react.ts";
 import Counter from "./components/Counter.tsx";
 import GHBadge from "./components/GHBadge.tsx";
+import "./shims-react.ts";
 
 export function App() {
-  return <div>
-    <link rel="stylesheet" href="styles.css"></link>
+  return (
+    <div>
+      <link rel="stylesheet" href="styles.css"></link>
 
-    <div className="App">
-      <header className="App-header">
-        <h1>React Deno Quickstart ⚛️ 🦕 📦</h1>
+      <div className="App">
+        <header className="App-header">
+          <h1>React Deno Quickstart ⚛️ 🦕 📦</h1>
 
-        <p>
-          Starter template for a React app built on Deno - including docs and CI
-        </p>
+          <p>
+            Starter template for a React app built on Deno - including docs and
+            CI
+          </p>
 
-        <GHBadge username="MichaelCurrin" repoName="react-deno-quickstart" />
-      </header>
+          <GHBadge username="MichaelCurrin" repoName="react-deno-quickstart" />
+        </header>
 
-      <Counter />
+        <Counter />
+      </div>
     </div>
-  </div>;
+  );
 }
