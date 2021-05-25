fb5eb18d5e16773a6bf8c58d3e92f06257bc188a
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-25 23:13:35 +0200

    Update Counter.tsx

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,8 @@
+/**
+ * Counter module.
+ */
 import { React } from "../../deps.ts";
 
-/** Warning - this works great in a static production build but locally the click does nothing. Not
- * even with console.log instead. **/
 export default function Counter() {
   // @ts-ignore Function is a named export but TS can't find it.
   const [count, setCount] = React.useState(0);
