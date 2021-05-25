c5b0253917a1b0af2962bf8e19bb1a21f3beed2b
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-25 22:45:36 +0200

    build: Update dev_deps.ts

--- dev_deps.ts
+++ dev_deps.ts
@@ -2,4 +2,3 @@
  * Development dependencies module.
  */
 export { Application } from "https://deno.land/x/abc@v1.3.1/mod.ts";
-export { default as ReactDOMServer } from "https://dev.jspm.io/react-dom@17.0.1/server";
