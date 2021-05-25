808b13b8949399530d1d0a4f485699256ef49f57
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-25 23:13:32 +0200

    Update src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,5 @@
 /**
  * Index module.
- *
- * When running a build, this is the entry point, which then uses `App` internally.
- *
- * When running a dev server, we can this skip minimal index module and use `App` directly as a root
- * path. That is based on a pattern I found and that works.
  */
 import { React, ReactDOM } from "../deps.ts";
 import { App } from "./App.tsx";
@@ -13,6 +8,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  // deno-lint-ignore no-undef
   document.getElementById("root"),
 );
